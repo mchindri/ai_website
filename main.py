265c9f474430bd9e627e3239e2ffb3ef4e47fa49
Mihail Chindris <mihail.chindris@analog.com>
2020-10-20 21:57:47 +0300

    Title only

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+
+#streamlit run main.py
+#streamlit run https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/app.py
+
+st.title('Up Farm (by Eccodictive) -- Geek 2020')
+
+
+
