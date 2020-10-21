b1b07b9bf6060d6850942ac7d78163a94b752ca5
Mihail Chindris <mihail.chindris@analog.com>
2020-10-21 23:11:57 +0300

    working prediction

--- main.py
+++ main.py
@@ -7,5 +7,34 @@ import streamlit as st
 
 st.title('Up Farm (by Eccodictive) -- Geek 2020')
 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
+class_names = ['healty', 'k_less']
+import matplotlib.pyplot as plt
+import numpy as np
 
+class_names = ['healty', 'k_less']
+
+img_path = '/content/drive/My Drive/samples/test_samples/img1.jpeg'
+img = keras.preprocessing.image.load_img(img_path)
+
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = img_array[0:50, 0:50,:]
+
+ni = np.array(img_array)
+
+st.image(ni.astype(int))
+
+model = tf.keras.models.load_model('/content/drive/My Drive/Geek/ai_model')
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+predictions = probability_model.predict(img_array)
+idx = np.argmax(predictions)
+if idx == 0:
+  st.text('Healthy plant')
+else:
+  st.text('Plant needs potasium')
